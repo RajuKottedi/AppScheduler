32082fe872c3492256aed045aa27e4351ec52e8e
Bangara raju <bkottedi@agility.com>
2018-09-03 11:54:39 +0300

    first commit

---
+++ AppWithScheduler/Code/SomeOtherTask.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppWithScheduler.Code
+{
+    public class SomeOtherTask : IScheduledTask
+    {
+        public string Schedule => "*/1 * * * *";
+
+        private readonly IServiceScopeFactory scopeFactory;
+
+        public SomeOtherTask(IServiceScopeFactory scopeFactory)
+        {
+            this.scopeFactory = scopeFactory;
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<IScheduledTask>();
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
